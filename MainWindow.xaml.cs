b7ee686e23c83342e2df66b7151dbaeb74551c42
Hieu Dong <donghuuhieu1520@gmail.com>
2020-12-23 19:39:57 +0700

    Fix layout, remove unused function

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,9 +17,6 @@ using System.Windows.Ink;
 
 namespace WpfControlAndApi
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
